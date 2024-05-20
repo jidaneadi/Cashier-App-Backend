b08014ef326f194ae6a1c6300fdef93a7a25d41d
adirama11 <jidaneadir11@gmail.com>
2024-05-20 14:56:58 +0700

    Update fitur create transaksi

--- controller/transaksicontroller/create.js
+++ controller/transaksicontroller/create.js
@@ -1,24 +1,54 @@
-const { Transaksi } = require('../../models')
-const Validator = require('fastest-validator')
-const v = new Validator()
+const { Transaksi, Keranjang } = require("../../models");
+const Validator = require("fastest-validator");
+const v = new Validator();
 
-module.exports = async(req, res) => {
-    const schema = {
-        id_pelanggan : "string | empty=true",
-        tot_harga : "string | empty=true"
-    }
+module.exports = async (req, res) => {
+  const schema = {
+    keranjang: {
+      type: "array",
+      items: {
+        type: "object",
+        props: {
+          id_barang: { type: "string", empty: false },
+          jumlah: { type: "string", empty: false },
+        },
+      },
+    },
+    id_karyawan: { type: "string", empty: false },
+    nama_pelanggan: { type: "string", empty: false },
+    tot_harga: { type: "string", empty: false },
+    layanan: { type: "string", empty: false },
+    pembayaran: { type: "string", empty: false },
+  };
 
-    const validation = v.validate(req.body, schema)
-    if(validation.length){
-        return res.status(400).json({msg:validation})
-    }
+  const validation = v.validate(req.body, schema);
+  if (validation.length) {
+    return res.status(400).json({ msg: validation });
+  }
 
-    const data = {
-        id_pelanggan : req.body.id_pelanggan,
-        tot_harga : req.body.tot_harga
-    }
+  //Mengecek jumlah data yang ada di variabel keranjang, apakah kosong atau tidak
+  if (!req.body.keranjang || req.body.keranjang.length === 0) {
+    res.status(400).send({ msg: "Keranjang belanja tidak boleh kosong!!!" });
+  }
 
-    await Transaksi.create(data)
+  //Menampung data untuk tabel Transaksi
+  const dataTransaksi = {
+    id_karyawan: req.body.id_karyawan,
+    nama_pelanggan: req.body.nama_pelanggan,
+    tot_harga: req.body.tot_harga,
+    layanan: req.body.layanan,
+    pembayaran: req.body.pembayaran,
+  };
 
-    return res.status(200).json({msg : "Transaksi berhasil"})
-}
+  const createTransaksi = await Transaksi.create(dataTransaksi);
+
+  const dataKeranjang = {
+    id_barang: createTransaksi.id,
+    id_transaksi: req.body.id_transaksi,
+    jumlah: req.body.jumlah,
+  };
+
+  const createKeranjang = await Keranjang.create(dataTransaksi);
+
+  return res.status(200).json({ msg: "Transaksi berhasil" });
+};
--- models/transaksi.js
+++ models/transaksi.js
@@ -1,48 +1,45 @@
 module.exports = (sequelize, DataTypes) => {
-    const Transaksi = sequelize.define("Transaksi", {
-        id :{
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-            allowNull: false
-        },
-        id_karyawan :{
-            type : DataTypes.INTEGER,
-        },
-        nama_pelanggan :{
-            type : DataTypes.INTEGER,
-        },
-        tot_harga : {
-            type : DataTypes.STRING,
-        },
-        pembayaran: {
-            type: DataTypes.ENUM,
-            values: [
-              "cash",
-              "qris",
-            ],
-            defaultValue: "cash",
-            allowNull: false,
-          },
-          layanan: {
-            type: DataTypes.ENUM,
-            values: [
-              "takeway",
-              "makan di tempat",
-            ],
-            defaultValue: "makan di tempat",
-            allowNull: false,
-          },
-        createdAt : {
-            type : DataTypes.DATE,
-        },
-        updatedAt : {
-            type : DataTypes.DATE,
-    },
+  const Transaksi = sequelize.define(
+    "Transaksi",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      id_karyawan: {
+        type: DataTypes.INTEGER,
+      },
+      nama_pelanggan: {
+        type: DataTypes.INTEGER,
+      },
+      tot_harga: {
+        type: DataTypes.STRING,
+      },
+      pembayaran: {
+        type: DataTypes.ENUM,
+        values: ["cash", "qris"],
+        defaultValue: "cash",
+        allowNull: false,
+      },
+      layanan: {
+        type: DataTypes.ENUM,
+        values: ["takeway", "makan di tempat"],
+        defaultValue: "makan di tempat",
+        allowNull: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+      },
     },
     {
-        tableName : "transaksi"
-    })
+      tableName: "transaksi",
+    }
+  );
 
-    return Transaksi;
-}
+  return Transaksi;
+};
