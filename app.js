bea83acc1e933683e033e50491aebd6ea65f2d68
adirama11 <jidaneadir11@gmail.com>
2024-06-13 18:22:32 +0700

    Update cors

--- app.js
+++ app.js
@@ -1,38 +1,41 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 // require('dotenv').config();
-require('dotenv').config({ path: '.env' })
-const cors = require('cors');
+require("dotenv").config({ path: ".env" });
+const cors = require("cors");
 
-const indexRouter = require('./routes/index');
-const productRouter = require('./routes/product');
-const keranjangRouter = require('./routes/keranjang');
-const transaksiRouter = require('./routes/transaksi');
-const karyawanRouter = require('./routes/karyawan');
-const loginRouter = require('./routes/index');
-const tokenRouter = require('./routes/token');
+const indexRouter = require("./routes/index");
+const productRouter = require("./routes/product");
+const keranjangRouter = require("./routes/keranjang");
+const transaksiRouter = require("./routes/transaksi");
+const karyawanRouter = require("./routes/karyawan");
+const loginRouter = require("./routes/index");
+const tokenRouter = require("./routes/token");
 const app = express();
 const corsOptions = {
-    origin: '*',
-    optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 200,
+  allowedHeaders: "Content-Type,Authorization",
+};
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/product', productRouter);
-app.use('/keranjang', keranjangRouter);
-app.use('/transaksi', transaksiRouter);
-app.use('/karyawan', karyawanRouter);
-app.use('/login', loginRouter);
-app.use('/refresh', tokenRouter);
+app.use("/", indexRouter);
+app.use("/product", productRouter);
+app.use("/keranjang", keranjangRouter);
+app.use("/transaksi", transaksiRouter);
+app.use("/karyawan", karyawanRouter);
+app.use("/login", loginRouter);
+app.use("/refresh", tokenRouter);
 
 module.exports = app;
 // app.listen(process.env.PORT);
